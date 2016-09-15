f3f25e029ae00955e160df689a651f013d8e6bd5
Aqel Ahammad <aqel123@gmail.com>
2016-09-15 22:03:00 +0530

    2D ready

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	private static void evaporation() {
 		
-		int[] n = {100, 100, 1};
+		int[] n = {2, 2, 2};
 		
 //		// D2Q9 Model
 //		int[][] c = { {0,0,0}, {1,0,0}, {0,1,0}, {-1,0,0}, {0,-1,0}, {1,1,0}, {-1,1,0}, {-1,-1,0}, {1,-1,0} };
@@ -24,27 +24,27 @@ public class Main {
 		Domain domain = new Domain(n, c);
 		
 		// Assign gi values in all the k directions
-//		Point.assignRandomPoints(domain, n, directions);
-		Point.defineCube(domain, n, 20);
+		Point.assignRandomPoints(domain, n, directions);
+//		Point.defineCube(domain, n, 20);
 		
-//		// Initial condition of phi = 0 at all points
-//		for (int i=0; i<n[0]; i++)
-//			for (int j=0; j<n[1]; j++)
-//				for (int k=0; k<n[2]; k++){
-//					double sign = 1.0;
-//					if (Math.random() < 0.5 ) sign = -1.0;
-//					domain.points[i][j][k].phi  = -1;
-//					
-//					domain.points[i][j][k].phi  = Math.random() * sign / 100.0;
-////					domain.points[i][j][k].phi = Double.parseDouble(String.format("%.8f",Math.random() * sign / 100.0 ));
-////					if (i<n[0]/2) domain.points[i][j][k].phi = 0.9999;
-////					else domain.points[i][j][k].phi = -0.9999;
-//				}
+		// Initial condition of phi = 0 at all points
+		for (int i=0; i<n[0]; i++)
+			for (int j=0; j<n[1]; j++)
+				for (int k=0; k<n[2]; k++){
+					double sign = 1.0;
+					if (Math.random() < 0.5 ) sign = -1.0;
+					domain.points[i][j][k].phi  = -1;
+					
+					domain.points[i][j][k].phi  = Math.random() * sign / 100.0;
+//					domain.points[i][j][k].phi = Double.parseDouble(String.format("%.8f",Math.random() * sign / 100.0 ));
+//					if (i<n[0]/2) domain.points[i][j][k].phi = 0.9999;
+//					else domain.points[i][j][k].phi = -0.9999;
+				}
 			
 		
 		
 		// Apply streaming function
-		for (int i=0; i<500; i++){
+		for (int i=0; i<15; i++){
 			
 			printPoints(domain, n);
 			domain.findNu();
