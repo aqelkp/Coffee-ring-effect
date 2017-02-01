3aef10382fd926d5e3805065561e2e9c3bbac15e
Aqel Ahammad <aqel123@gmail.com>
2017-02-01 22:50:06 +0530

    Evaporation implemented - nu v/s z verification done

--- src/DataVisuals.java
+++ src/DataVisuals.java
@@ -11,7 +11,7 @@ public class DataVisuals {
 	public static void plotBoundaryPhi(Domain domain, int[] n, double[] xaxis, double[] yaxis, int i) {
 		// TODO Auto-generated method stub
 		findYAxis(domain, n, yaxis);
-		XYChart chart = QuickChart.getChart("Phi v/s r at t = " + i, "r", "phi", "phi(x)", xaxis, yaxis);
+		XYChart chart = QuickChart.getChart("Nu v/s x at t = " + i, "x", "nu", "nu(x)", xaxis, yaxis);
 		
 //		findTanh(domain, n, yaxis);
 //		chart.addSeries("tanh(r/epsilon)", xaxis, yaxis);
@@ -19,7 +19,7 @@ public class DataVisuals {
 		new SwingWrapper(chart).displayChart();
 		
 		try {
-			BitmapEncoder.saveBitmap(chart, "images/Phi_vs_r_at_t=" + i, BitmapFormat.PNG);
+			BitmapEncoder.saveBitmap(chart, "images/nu_vs_x_at_t=" + i, BitmapFormat.PNG);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,7 +44,7 @@ public class DataVisuals {
 	public static void findYAxis(Domain domain, int[] n, double[] yaxis) {
 		int center = n[0]/2;
 		for (int i =0; i<n[1]; i++){
-			yaxis[i] = domain.phi[center][i][0];
+			yaxis[i] = domain.nu[center][i][0];
 		}
 	}
 
